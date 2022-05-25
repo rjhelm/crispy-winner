c8e7c42e0624a3cc88cadbe3a5179e36c9377d08
rjhelm <rjhelm@github.io>
2022-05-24 20:02:45 -0600

    created dashboard component

---
+++ frontend/src/components/Dashboard/Dashboard.tsx
@@ -0,0 +1,64 @@
+import React, { useEffect } from 'react';
+import { loadUserData, updateActiveState, updateActiveUserList } from '../../actions'
+import { useDispatch, useSelector } from 'react-redux';
+import createHashHistory from '../../history';
+import Sidebar from '../Sidebar/Sidebar';
+import SendMessage from '../SendMessage/SendMessage';
+import Header from '../Header/Header';
+import Messages from '../Messages/Messages';
+import ActiveUserList from '../ActiveUserList/ActiveUserList';
+import { StoreState } from '../../reducers';
+
+export default function Dashboard() {
+    const user = useSelector((state: StoreState) => state.user);
+    const { activeServer } = useSelector((state: StoreState) => state.chat);
+    const dispatch = useDispatch();
+
+    // Ping server every 5 minutes to update our active status
+    // also fetches new lost of active users
+    const updateActiveStatus = () => {
+        dispatch(updateActiveState());
+        dispatch(updateActiveUserList(activeServer.split('-')[1]));
+        setTimeout(updateActiveStatus, 5 * 60000);
+    }
+
+    // Listens for changes on isSignedIn
+    // gets initial user data upon change
+    useEffect(() => {
+        if (!user.isSignedIn) {
+            createHashHistory.push('/');
+        } else {
+            dispatch(loadUserData(user.userId));
+            updateActiveStatus();
+        } // eslint-disable-next-line
+    }, [dispatch, user.isSignedIn, user.userId]);
+
+    // Watches viewport height
+    window.addEventListener('resize', () => {
+        let vh = window.innerHeight * 0.01;
+        document.documentElement.style.setProperty('--vh', `${vh}px`);
+    });
+
+    return (
+        <div className="dashboard">
+            <div className="grid-container">
+                <div className="sidebar-grid">
+                    <Sidebar />
+                </div>
+
+                <div className="messages-grid">
+                    <Header />
+                    <Messages />
+                </div>
+
+                <div className="user-list-grid">
+                    <ActiveUserList />
+                </div>
+
+                <div className="send-messages-grid">
+                    <SendMessage />
+                </div>
+            </div>
+        </div>
+    )
+}
